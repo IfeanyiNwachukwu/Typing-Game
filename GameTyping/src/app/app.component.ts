df19d2ef08a2b1b17bf66d84311b4750186e7243
Ifeanyi Nwachukwu <ifeanyin@xceed365.com>
2022-06-10 09:37:49 +0100

    Project complete

--- GameTyping/src/app/app.component.ts
+++ GameTyping/src/app/app.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
 
+import {randPhrase} from '@ngneat/falso'
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'GameTyping';
+   randomText = randPhrase();
+   enteredText = '';
+   
+
+   onInput(value:string){
+     this.enteredText = value;
+   }
+   compare(randomLetter:string,enteredLetter:string){
+      if(!enteredLetter){
+        return 'pending';
+      }
+      
+      return randomLetter === enteredLetter ? 'correct' : 'incorrect'
+   }
 }
